a68c943bc0485bacef0fd2259358ca04b0fab498
Murilo Lopes <mloopes11@gmail.com>
2021-04-07 20:06:44 -0300

    Entendendo IMUTABILIDADE

--- 01-github-explorer/src/components/Counter.jsx
+++ 01-github-explorer/src/components/Counter.jsx
@@ -1,6 +1,15 @@
 import { useState } from "react";
 //Sempre que uma funçção que estamos importanto começar com use, ela é chamada
 //Hook -> gancho
+
+//imutabilidade
+
+//usuarios = ['usuario1', 'usuario2', 'usuario3']
+
+//usuarios.push('usuaio4') !!!ERRADO!!!
+
+//novosUsuarios = [...usuarios, 'usuario4'] CORRETO
+
 export function Counter() {
   const [counter, setCounter] = useState(0);
   function incremment() {
