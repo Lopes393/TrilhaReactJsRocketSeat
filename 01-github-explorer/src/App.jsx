2796cf8b9406a692159ae44857aecc5ab159949c
Murilo Lopes <mloopes11@gmail.com>
2021-04-07 22:37:30 -0300

    Style Repository List

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,11 +1,5 @@
-import { Counter } from "./components/Counter";
 import { RepositoryList } from "./components/RepositoryList";
 import "./styles/global.scss";
 export function App() {
-  return (
-    <>
-      <RepositoryList />
-      <Counter />
-    </>
-  );
+  return <RepositoryList />;
 }
--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,15 +1,21 @@
 import { RepositoryItem } from "./RepositoryItem";
+import "../styles/repositories.scss";
 
+const repository = {
+  name: "unform",
+  description: "Forms in React",
+  link: "https://github.com/Lopes393",
+};
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository="unformed2" />
-        <RepositoryItem />
-        <RepositoryItem />
-        <RepositoryItem />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   );
