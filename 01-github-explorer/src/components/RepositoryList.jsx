725c2059011abf8e8b4a7f9adbf2d64c569c3baa
Murilo Lopes <mloopes11@gmail.com>
2021-04-07 23:16:22 -0300

    Listagem de repositorios do github finalizada

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -2,12 +2,6 @@ import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss";
 import { useState, useEffect } from "react";
 
-//https://api.github.com/users/Lopes393/repos
-const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "https://github.com/Lopes393",
-};
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
@@ -21,10 +15,11 @@ export function RepositoryList() {
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
+        })}
       </ul>
     </section>
   );
